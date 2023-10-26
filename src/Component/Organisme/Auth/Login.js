61a6781c956799a508e4068f954cf5e3267b3fac
selemene112 <ariyanda98y@gmail.com>
2023-10-26 21:13:33 +0800

    modified v1.1.1 modified Component organisme for login

--- src/Component/Organisme/Auth/Login.js
+++ src/Component/Organisme/Auth/Login.js
@@ -1,19 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
 import TextInputComponent from '../../Atom/TextInput';
 import ButtonComponent from '../../Atom/ButtonComponent';
+import {useNavigation} from '@react-navigation/native';
+
 import {View} from 'react-native';
+import {useDispatch} from 'react-redux';
+
+import {loginUser} from '../../../Redux/Actions/Auth/LoginAction';
 
 function LoginOrganisme() {
+  const navigation = useNavigation();
+
+  const dispatch = useDispatch();
+  const [inputData, setInputData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const postDataLogin = async () => {
+    dispatch(loginUser(inputData))
+      .then(response => {
+        if (response) {
+          navigation.navigate('CardRecipe');
+        }
+      })
+      .catch(error => {
+        console.error('Gagal melakukan login:', error);
+      });
+  };
+
+  const onChangeLogin = (name, value) => {
+    setInputData({...inputData, [name]: value});
+  };
+
   return (
     <View>
-      <TextInputComponent placeholder="Username" />
-      <TextInputComponent placeholder="Password" isPassword={true} />
+      <TextInputComponent
+        placeholder="Email"
+        onChangeText={value => onChangeLogin('email', value)}
+        value={inputData.email}
+      />
+      <TextInputComponent
+        placeholder="Password"
+        isPassword={true}
+        onChangeText={value => onChangeLogin('password', value)}
+        value={inputData.password}
+      />
       <ButtonComponent
         title="Login"
         marginBottom={10}
-        onPress={() => {
-          // Tambahkan kondisi login
-        }}
+        onPress={() => postDataLogin()}
       />
     </View>
   );
