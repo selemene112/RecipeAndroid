60c1f36632adf3078a18ec0c06b9f687433fb2f4
selemene112 <ariyanda98y@gmail.com>
2023-11-02 07:57:05 +0800

    add Search Atom COmponet

---
+++ src/Component/Atom/SearchComponent.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {StyleSheet, TextInput, View} from 'react-native';
+
+const styles = StyleSheet.create({
+  input_search: {
+    padding: 20,
+    backgroundColor: '#e5e5e5',
+    borderRadius: 15,
+    fontFamily: 'Poppins-Medium',
+    color: '#aaaaaa',
+  },
+});
+
+const SearchBar = props => {
+  return (
+    <View>
+      <TextInput
+        onChangeText={props.changeText}
+        style={styles.input_search}
+        placeholder="Search Pasta, Bread, etc"
+        placeholderTextColor="#aaaaaa"
+      />
+    </View>
+  );
+};
+
+export default SearchBar;
